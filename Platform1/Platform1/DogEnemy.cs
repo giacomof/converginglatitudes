03678be9ca39ac8f9f350a68779fb88de2f7cafa
Telivyel <Telivyel@f1359c93-47bb-ce26-f8fc-f251cb3f0622>
2010-11-04 10:23:51 +0000

    started dog enemy adding

---
+++ Platform1/Platform1/DogEnemy.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LearningXNA
+{
+    /// <summary>
+    /// A monster who is impeding the progress of monster-cat.
+    /// </summary>
+    class DogEnemy
+    {
+        public Level Level
+        {
+            get { return level; }
+        }
+        Level level;
+
+        /// <summary>
+        /// Position in world space of the bottom center of this dog.
+        /// </summary>
+        public Vector2 Position
+        {
+            get { return position; }
+        }
+        Vector2 position;
+
+        private Rectangle localBounds;
+        /// <summary>
+        /// Gets a rectangle which bounds this dog in world space.
+        /// </summary>
+        public Rectangle BoundingRectangle
+        {
+            get
+            {
+                int left = (int)Math.Round(Position.X - sprite.Origin.X) + localBounds.X;
+                int top = (int)Math.Round(Position.Y - sprite.Origin.Y) + localBounds.Y;
+
+                return new Rectangle(left, top, localBounds.Width, localBounds.Height);
+            }
+        }
+
+        // Animations
+        private Animation idleAnimation;
+        private AnimationPlayer sprite;
+
+        /// <summary>
+        /// The direction this dog is facing and moving along the X axis.
+        /// </summary>
+        private FaceDirection direction = FaceDirection.Left;
+
+        /// <summary>
+        /// How long this dog has been waiting before turning around.
+        /// </summary>
+        private float waitTime;
+
+        /// <summary>
+        /// How long to wait before turning around.
+        /// </summary>
+        private const float MaxWaitTime = 0.5f;
+
+        /// <summary>
+        /// The speed at which this dog moves along the X axis.
+        /// </summary>
+
+        private const float MoveSpeed = 128.0f;
+
+        /// <summary>
+        /// Constructs a new dog.
+        /// </summary>
+        public DogEnemy(Level level, Vector2 position)
+        {
+            this.level = level;
+            this.position = position;
+
+            LoadContent();
+        }
+
+        /// <summary>
+        /// Loads a particular dog sprite sheet and sounds.
+        /// </summary>
+        public void LoadContent()
+        {
+            // Load animations.
+            idleAnimation = new Animation(Level.Content.Load<Texture2D>("Sprites/EnemyDog/Idle"), 0.15f, true);
+            sprite.PlayAnimation(idleAnimation);
+
+            // Calculate bounds within texture size.
+            int width = (int)(idleAnimation.FrameWidth * 0.35);
+            int left = (idleAnimation.FrameWidth - width) / 2;
+            int height = (int)(idleAnimation.FrameWidth * 0.7);
+            int top = idleAnimation.FrameHeight - height;
+            localBounds = new Rectangle(left, top, width, height);
+        }
+
+
+        /// <summary>
+        /// Paces back and forth along a platform, waiting at either end.
+        /// </summary>
+        public void Update(GameTime gameTime)
+        {
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Calculate tile position based on the side we are walking towards.
+            float posX = Position.X + localBounds.Width / 2 * (int)direction;
+            int tileX = (int)Math.Floor(posX / Tile.Width) - (int)direction;
+            int tileY = (int)Math.Floor(Position.Y / Tile.Height);
+
+            
+        }
+
+        /// <summary>
+        /// Draws the animated dog.
+        /// </summary>
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // Stop running when the game is paused or before turning around.
+            if (!Level.Player.IsAlive ||
+                Level.ReachedExit ||
+                Level.TimeRemaining == TimeSpan.Zero ||
+                waitTime > 0)
+            {
+                sprite.PlayAnimation(idleAnimation);
+            }
+
+
+            // Draw facing the way the dog is moving.
+            SpriteEffects flip = direction > 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            sprite.Draw(gameTime, spriteBatch, Position, flip);
+        }
+    }
+}
