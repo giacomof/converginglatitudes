3b3a44c2e83f04a6d8254299837f0820496d3f64
Telivyel <Telivyel@f1359c93-47bb-ce26-f8fc-f251cb3f0622>
2010-11-30 13:13:08 +0000

    fixed movable platform able to go "down" or "up" forever

--- Platform1/Platform1/MovableTile.cs
+++ Platform1/Platform1/MovableTile.cs
@@ -109,7 +109,8 @@ namespace LearningXNA
                     //If we're about to run into a wall that isn't a MovableTile move in other direction.  
                     if (Level.GetCollision(tileX + (int)direction, tileY) == TileCollision.Impassable ||
                         Level.GetCollision(tileX + (int)direction, tileY) == TileCollision.Platform ||
-                        Level.GetCollision(tileX + (int)direction, tileY) == TileCollision.PlatformCollider)
+                        Level.GetCollision(tileX + (int)direction, tileY) == TileCollision.PlatformCollider ||
+                        Level.GetCollision(tileX, tileY) == TileCollision.LevelFrame)
                     {
                         velocity = new Vector2(0.0f, 0.0f);
                         waitTime = MaxWaitTime;
