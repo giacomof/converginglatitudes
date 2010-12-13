e4a4a6212295d17ca6997bf747636bbf3b060c76
Ronny.J.Nilsson@gmail.com <Ronny.J.Nilsson@gmail.com@f1359c93-47bb-ce26-f8fc-f251cb3f0622>
2010-12-13 12:59:41 +0000

    added level 4

--- Platform1/Platform1/PlatformerGame.cs
+++ Platform1/Platform1/PlatformerGame.cs
@@ -106,7 +106,7 @@ namespace LearningXNA
         private SoundEffect youlose3;
         private SoundEffect youlose4;
 
-        private int levelIndex = 5;
+        private int levelIndex = 2;
 
 
         private Level level;
